ac309c31cd3e1f8e99e1d9fd7e786806990f83ea
Meruemxplgx <126265885+Meruemxplgx@users.noreply.github.com>
2023-06-18 16:31:12 +0600

    Add files via upload

---
+++ Controllers/ContactsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+using Microsoft.AspNetCore.Mvc;
+using shop_test.Models;
+
+namespace shop_test.Controllers
+{
+    public class ContactsController : Controller
+    {
+        public IActionResult Index() // Contacts
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Check(Contact contact)
+        {
+            if(ModelState.IsValid) 
+            {
+                return Redirect("/");
+            }
+
+            return View("Index");
+        }
+    }
+}
